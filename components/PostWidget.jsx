ea4c2bc5a3950a32c78eeb56b89276889217df94
Thies <thies@fishonfire.com>
2023-10-11 17:27:05 +0200

    Complete layout refactor

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import moment from 'moment';
+// import moment from 'moment';
 import Link from 'next/link';
 
-import { grpahCMSImageLoader } from '../util';
 import { getSimilarPosts, getRecentPosts } from '../services';
+import { FormatDate } from '../utils/formatDate';
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
@@ -23,26 +22,18 @@ const PostWidget = ({ categories, slug }) => {
 
   return (
     <div className="glass_card shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
-      {relatedPosts.map((post, index) => (
-        <div key={index} className="flex items-center w-full mb-4">
-          <div className="w-16 flex-none">
-            <Image
-              loader={grpahCMSImageLoader}
-              alt={post.title}
-              height="60px"
-              width="60px"
-              unoptimized
-              className="align-middle rounded-full"
-              src={post.featuredImage.url}
-            />
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Gerelateerde Posts' : 'Recente Posts'}</h3>
+      {relatedPosts.map((post, index) => {
+        const bgColor = post.categories[0].slug === 'stage-1' ? 'stage-1-hover' : 'stage-2-hover';
+        return (
+          <div key={index} className="flex items-center w-full mb-4">
+            <div className="flex-grow">
+              <p className="text-gray-500 font-xs">{FormatDate(post.postDate)}</p>
+              <Link href={`/post/${post.slug}`} key={index}><span className={`text-md cursor-pointer ${bgColor}`}>{`${post.title} | ${post.categories[0].name}`}</span></Link>
+            </div>
           </div>
-          <div className="flex-grow ml-4">
-            <p className="text-gray-500 font-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
-            <Link href={`/post/${post.slug}`} className="text-md" key={index}>{post.title}</Link>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
