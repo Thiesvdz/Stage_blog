e120941d5a2b77460216426ddbdb8d036c394dee
Thies <thies@fishonfire.com>
2024-01-23 21:20:07 +0100

    Refactored comment code

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -21,12 +21,14 @@ export default async function asynchandler(req, res) {
     }
   `;
 
-  const result = await graphQLClient.request(query, {
-    name: req.body.name,
-    email: req.body.email,
-    comment: req.body.comment,
-    slug: req.body.slug,
-  });
+  try {
+    const result = await graphQLClient.request(query, {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment,
+      slug: req.body.slug,
+    });
+    return res.status(200).send(result);
+  } catch (error) { console.log(error); }
 
-  return res.status(200).send(result);
 }
